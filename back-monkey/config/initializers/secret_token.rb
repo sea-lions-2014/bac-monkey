542d3ac220785796871ad2d585c41f7172668350
Apprentice <apprentice@dbc22.local>
2014-02-27 10:57:20 -0800

    Adds rails skeleton

---
+++ back-monkey/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BackMonkey::Application.config.secret_token = 'df4d1eb0aa5e850f8d629b4d1857176f5580b191a156be74dbe03b6cc09b26aa40ab624c6d0233469966e59ea02d37eefdc426e0ae40bb483498563770cd67e4'
