76b49b3f4e3646a56ad9714f9edb6d7ae5cf2fbd
Darcey Lachtman & Patrick Vilhena <pair+dslachtman.nifengle@devbootcamp.com>
2014-02-27 15:41:31 -0800

    Implements multi-line chart.
    Todo: Edit tooltip

--- back-monkey/app/assets/javascripts/linechart.js
+++ back-monkey/app/assets/javascripts/linechart.js
@@ -1,93 +1,64 @@
-// /*These lines are all chart setup.  Pick and choose which chart features you want to utilize. */
-// nv.addGraph(function() {
-//   var chart = nv.models.lineChart()
-//                 .margin({left: 100})  //Adjust chart margins to give the x-axis some breathing room.
-//                 .useInteractiveGuideline(true)  //We want nice looking tooltips and a guideline!
-//                 .transitionDuration(350)  //how fast do you want the lines to transition?
-//                 .showLegend(true)       //Show the legend, allowing users to turn on/off line series.
-//                 .showYAxis(true)        //Show the y-axis
-//                 .showXAxis(true)        //Show the x-axis
-//   ;
 
-//   chart.xAxis     //Chart x-axis settings
-//       .axisLabel('Time (ms)')
-//       .tickFormat(d3.format(',r'));
 
-//   chart.yAxis     //Chart y-axis settings
-//       .axisLabel('Voltage (v)')
-//       .tickFormat(d3.format('.02f'));
-
-//   /* Done setting the chart up? Time to render it!*/
-//   var myData = sinAndCos();   //You need data...
-
-//   d3.select('#chart svg')    //Select the <svg> element you want to render the chart in.
-//       .datum(myData)         //Populate the <svg> element with chart data...
-//       .call(chart);          //Finally, render the chart!
-
-//   //Update the chart when window resizes.
-//   nv.utils.windowResize(function() { chart.update() });
-//   return chart;
-// });
-// /**************************************
-//  * Simple test data generator
-//  */
-// function sinAndCos() {
-//   var sin = [],sin2 = [],
-//       cos = [];
-
-//   //Data is represented as an array of {x,y} pairs.
-//   for (var i = 0; i < 100; i++) {
-//     sin.push({x: i, y: Math.sin(i/10)});
-//     sin2.push({x: i, y: Math.sin(i/10) *0.25 + 0.5});
-//     cos.push({x: i, y: .5 * Math.cos(i/10)});
-//   }
-
-//   //Line chart data should be sent as an array of series objects.
-//   return [
-//     {
-//       values: sin,      //values - represents the array of {x,y} data points
-//       key: 'Sine Wave', //key  - the name of the series.
-//       color: '#ff7f0e'  //color - optional: choose your own line color.
-//     },
-//     {
-//       values: cos,
-//       key: 'Cosine Wave',
-//       color: '#2ca02c'
-//     },
-//     {
-//       values: sin2,
-//       key: 'Another sine wave',
-//       color: '#7777ff',
-//       area: true      //area - set to true if you want this line to turn into a filled area chart.
+// function myData() {
+//     var series1 = [];
+//     var series2 = [];
+//     for(var i =1; i < 12; i ++) {
+//         series1.push({
+//             x: i, y: i*2
+//         });
+//         series2.push({
+//           x: i, y: (100 * i)/10
+//         });
 //     }
-//   ];
+
+//     return [
+//         {
+//             key: "Series #1",
+//             values: series1,
+//             color: "#0000ff"
+//         },
+//         {
+//           key: "Series #2",
+//           values: series2,
+//           color: "#222"
+//         }
+//     ];
 // }
 
-function myData() {
-    var series1 = [];
-    var series2 = [];
-    for(var i =1; i < 12; i ++) {
-        series1.push({
-            x: i, y: 100 / i
-        });
-        series2.push({
-          x: i, y: (100 * i)/10
-        });
-    }
 
-    return [
-        {
-            key: "Series #1",
-            values: series1,
-            color: "#0000ff"
-        },
-        {
-          key: "Series #2",
-          values: series2,
-          color: "#222"
-        }
-    ];
+function generateSample(decayRate){
+  var dataObj = []
+  var substance = 1
+
+  for(var i =0; i<12; i+=0.25){
+    dataObj.push({x: i*60, y: substance})
+    substance *= substance * decayRate
+  }
+  return dataObj
 }
+var sampleData = generateSample(0.99)
+var sampleData2 = generateSample(0.93)
+var sampleData3 = generateSample(0.91)
+
+var dataObj = [
+  {
+    key: "BAC",
+    values: sampleData,
+    color: 'yellow'
+  },
+  {
+    key: "Caffeine",
+    values: sampleData2,
+    color: 'red'
+  },
+  {
+    key: "THC",
+    values: sampleData3,
+    color: 'green'
+  }
+]
+
 
 nv.addGraph(function() {
     var chart = nv.models.lineChart()
@@ -96,21 +67,23 @@ nv.addGraph(function() {
                 .transitionDuration(350)  //how fast do you want the lines to transition?
                 .showLegend(true)       //Show the legend, allowing users to turn on/off line series.
                 .showYAxis(true)        //Show the y-axis
-                .showXAxis(true)        //Show the x-axis
+                .showXAxis(false)        //Show the x-axis
 ;
+
 
     chart.xAxis
         .axisLabel("Time")
-        // .tickformat(d3.format("d"))
         ;
 
     chart.yAxis
-        .axisLabel("Y-axis Label")
-        .tickFormat(d3.format("d"))
+        .axisLabel("BAC")
+        .tickFormat(d3.format(".4s"))
         ;
 
+    chart.forceY([0,0.8])
+
     d3.select("svg")
-        .datum(myData())
+        .datum(dataObj)
         .transition().duration(500).call(chart);
 
     nv.utils.windowResize(
