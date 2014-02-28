038747c7c0a97617c7a662f0f9396f6ca204a66d
John Olmsted <apprentice@dbc22.local>
2014-02-28 10:28:56 -0800

    Changed class method notation in AlcoholCalc
    Switched from 'self' to 'class << self; end'

---
+++ back-monkey/app/models/caffeine_calc.rb
@@ -0,0 +1,23 @@
+#-----METABOLIZATION OF CAFFEINE-----
+
+# There is no agreed formula for estimating the mg of caffeine present in the bloodstream.
+# However, the elimination half-life has been estimated. One study reported an average of
+# 4.9 hours among healthy adults, another study reported 6 hours. We will take the mean of
+# these two studies and assume an average elimination half-life of 5.5 hours.
+
+#Let
+# milligrams = milligrams of caffeine consumed
+# hours = hours passed since consumption
+# Assume an hourly elimination rate of 9 %
+
+# current dose = milligrams – (milligrams * (0.09 * hours))
+
+# Relevant study:
+# http://www.ncbi.nlm.nih.gov/pmc/articles/PMC2995791/
+
+class CaffeineCalc
+
+  class << self
+
+  end
+end
