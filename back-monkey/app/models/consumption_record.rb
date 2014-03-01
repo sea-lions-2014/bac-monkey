0ddd12810481157314ee1c4059b1450ba17472c2
John Olmsted <johnaolmsted@gmail.com>
2014-03-01 14:42:19 -0800

    Adds #update_user_total callback

--- back-monkey/app/models/consumption_record.rb
+++ back-monkey/app/models/consumption_record.rb
@@ -1,9 +1,18 @@
 class ConsumptionRecord < ActiveRecord::Base
   belongs_to :user
+  before_save :update_user_total
 
   def dose_description
     desc = "#{self.amount} #{self.unit_of_measure}"
     desc += "s" unless self.amount == 1
-    desc
+    desc 
+  end
+
+  def update_user_total
+    if self.user
+      self.user.total_alcohol += self.amount if self.substance == "alcohol"
+      self.user.total_caffeine += self.amount if self.substance == "caffeine"
+      self.user.total_nicotine += self.amount if self.substance == "nicotine"
+    end
   end
 end
