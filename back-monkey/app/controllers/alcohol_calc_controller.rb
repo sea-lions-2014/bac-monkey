f8f2e3f2136b2a9313c3223a124d4c222bf5b7c9
Patrick Vilhena <null>
2014-02-28 17:59:36 -0800

    updated controllers to account for new model format

--- back-monkey/app/controllers/alcohol_calc_controller.rb
+++ back-monkey/app/controllers/alcohol_calc_controller.rb
@@ -1,12 +1,19 @@
 class AlcoholCalcController < ApplicationController
   respond_to :json, only: [:create]
+  before_filter :format_args_data
+
   def create
+    alcohol_calc = AlcoholCalc.new
+    @bac = alcohol_calc.bac_series(args)
+    render json: @bac
+  end
+
+  protected
+
+  def format_args_data
     args = params[:alcohol_calc]
     args[:weight] = args[:weight].to_i
     args[:alcohol] = args[:drinks].to_i * 0.6
     args[:hours] = args[:hours].to_i
-
-    @bac = AlcoholCalc.bac_series(args)
-    render json: @bac
   end
 end
