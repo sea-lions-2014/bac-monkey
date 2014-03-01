c9c6b35eb29941c0f6d0933421ccf7e591eade82
Patrick Vilhena <null>
2014-02-28 16:23:35 -0800

    Created caffeine form, controller, and specs.
    Implements changing between forms on index page

--- back-monkey/app/assets/javascripts/getdata.js
+++ back-monkey/app/assets/javascripts/getdata.js
@@ -1,19 +1,32 @@
 $(function(){
-  $('#alc_form').on('ajax:success', {args: $(this).serialize()}, formatData)
+  $('#alc_form').on('ajax:success', {args: $(this).serialize()}, formatBACData)
+  $('#caffeine_form').on('ajax:success', {args: $(this).serialize()}, formatBACData)
+  $('#nicotine_form').on('ajax:success', {args: $(this).serialize()}, formatBACData)
 });
 
 
-function formatData(event, data, status, xhr){
+function formatBACData(event, data, status, xhr){
+  formatData(data, 'BAC')
+}
+
+function formatCaffeineData(event, data, status, xhr){
+  formatData(data, 'Caffeine')
+}
+
+function formatNicotineData(event, data, status, xhr){
+  formatData(data, 'Nicotine')
+}
+
+function formatData(data, key){
   var values = []
   for (var i=0; i< data.length; i++){
     values.push({x: data[i][0]*60, y: data[i][1]});
   }
   var dataObj = [
     {
-      key: "BAC",
+      key: key,
       values: values
     }
   ]
-
   drawChart(dataObj);
 }
