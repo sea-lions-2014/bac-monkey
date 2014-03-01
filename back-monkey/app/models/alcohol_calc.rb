86b5fbd3b6b14e910fd0b399f3d07812c0726577
John Olmsted <apprentice@dbc21.local>
2014-02-28 17:43:52 -0800

    Refactors by separating concerns into smaller methods

--- back-monkey/app/models/alcohol_calc.rb
+++ back-monkey/app/models/alcohol_calc.rb
@@ -24,57 +24,42 @@ class AlcoholCalc
     @hours = args[:hours]
     @interval_count = args[:interval_count] || 49
   end
+
   # Given the amount of alcohol consumed over a period of time, returns a
   # data-point series of BAC percentages at 15 minute intervals as a
-  # nested array. The argument hash should have the following:
-  # {
-  #   starting_bac: defaults to 0,
-  #   alcohol: ounces of pure alcohol,
-  #   weight: in pounds,
-  #   gender: "m" or "f",
-  #   hours: time spent continually drinking, in intervals of 0.25
-  #   interval_count: defaults to 49 (12 hours of data-points)
-  # }
+  # nested array.
   def bac_series
     build_up = bac_build_up
-    last_bac = build_up.last.last
-    last_drinking_hour = build_up.last.first
-    break_down = bac_break_down({
-      starting_bac: last_bac,
-      starting_hour: last_drinking_hour
-    })
-
+    break_down = bac_break_down
     build_up.pop
     build_up + break_down
   end
 
+  private
 
   # Returns BAC series during the time you are continually drinking
   def bac_build_up
-    current_bac = @current_bac
-    build_up_interval_count = (@hours / 0.25).to_i
-    consumption_rate = @alcohol / build_up_interval_count.to_f
+    next_bac = @current_bac
     bac_series = [[0, @current_bac]]
 
     build_up_interval_count.times do |interval|
-      current_bac = (current_bac + bac(consumption_rate, @weight, @ratio)) - metabolized
-      current_bac = 0 if @current_bac < 0
-      bac_series << [interval * 0.25 + 0.25, current_bac.round(4)]
+      # next_bac = next_bac_up(next_bac)
+      next_bac = (next_bac + bac(consumption_rate, @weight, @ratio)) - metabolized
+      next_bac = 0 if next_bac < 0
+      bac_series << [interval * 0.25 + 0.25, next_bac.round(4)]
     end
 
     bac_series
   end
 
   # Returns a BAC series during the time you are not drinking
-  def bac_break_down(args)
-    starting_bac = args[:starting_bac] || 0
-    starting_hour = args[:starting_hour] || 0
+  def bac_break_down
     bac_series = []
 
     @interval_count.times do |interval|
       hours = interval * 0.25
-      next_bac = next_bac(starting_bac, hours)
-      bac_series << [starting_hour + hours, next_bac]
+      next_bac = next_bac_down(peak_bac, hours)
+      bac_series << [last_drinking_hour + hours, next_bac]
     end
 
     bac_series
@@ -90,15 +75,36 @@ class AlcoholCalc
     (0.015 * 0.25)
   end
 
-  # Given a starting BAC, returns BAC after n hours have passed
-  def next_bac(starting_bac, hours)
-    next_bac = (starting_bac - (0.015 * hours)).round(4)
-    next_bac < 0 ? 0 : next_bac
-  end
-
   # Returns alcohol distribution ratio based on gender
   def ratio(gender)
     return 0.73 if gender == "m"
     return 0.66 if gender == "f"
+  end
+
+  # def next_bac_up(next_bac)
+  #   (next_bac + bac(consumption_rate, @weight, @ratio)) - metabolized
+  #   next_bac = 0 if next_bac < 0
+  #   next_bac
+  # end
+
+  def next_bac_down(starting_bac, hours)
+    next_bac = (starting_bac - (0.015 * hours)).round(4)
+    next_bac < 0 ? 0 : next_bac
+  end
+
+  def peak_bac
+    bac_build_up.last.last
+  end
+
+  def last_drinking_hour
+    bac_build_up.last.first
+  end
+
+  def build_up_interval_count
+    (@hours / 0.25).to_i
+  end
+
+  def consumption_rate
+    @alcohol / build_up_interval_count.to_f
   end
 end
