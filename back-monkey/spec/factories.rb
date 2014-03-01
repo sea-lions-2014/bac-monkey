c1551304118be04329f4f95b9ca3aa5a55e5b2d3
Patrick Vilhena <plvilhena@gmail.com>
2014-03-01 14:21:20 -0800

    fixed user specs

--- back-monkey/spec/factories.rb
+++ back-monkey/spec/factories.rb
@@ -2,16 +2,16 @@ FactoryGirl.define do
   factory :user do params =
     user_name "David"
     email "email@yesanemail.com"
-    password_hash BCrypt::Password.create("password")
-    gender 'Male'
+    password_digest BCrypt::Password.create("password")
+    gender 'm'
     weight 123
   end
 
   factory :non_user, class: User do
     user_name "David"
     email "notanemail"
-    password_hash "password"
-    gender 'Male'
+    password_digest "password"
+    gender 'm'
     weight 123
   end
 
