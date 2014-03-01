c49106d21179ed4f0c0b9564fb923a3db9f5f339
John Olmsted <apprentice@dbc21.local>
2014-02-28 17:43:52 -0800

    Refactors by separating concerns into smaller methods

--- back-monkey/app/models/substance_calc.rb
+++ back-monkey/app/models/substance_calc.rb
@@ -1,9 +1,5 @@
 class SubstanceCalc
-  class << self
-
-    #-----CLASS INSTANCE VARIABLES-----
-
-    # THESE SHOULD BE ASSIGNED IN SUBCLASSES
+    #-----THESE SHOULD BE ASSIGNED IN SUBCLASSES-----
 
     # Rate at which a substance is eliminated from the body per hour.
     # Should be a percentage (e.g. 0.12 for 12%).
@@ -14,7 +10,7 @@ class SubstanceCalc
     # precision.
     @precision
 
-    #----------------------------------
+    #------------------------------------------------
 
     # Given the amount of substance consumed over a period of time, mg_series
     # returns a data-point series of mg concentrations at 15 minute intervals
@@ -25,20 +21,17 @@ class SubstanceCalc
     #   hours: time spent continually consuming the substance, in intervals of 0.25
     #   interval_count: optional, defaults to 49 (12 hours of data-points)
     # }
-    def mg_series(args)
-      current_mg = args[:current_mg] || 0
-      milligrams = args[:milligrams]
-      hours = args[:hours]
-      interval_count = args[:interval_count] || 49
 
-      build_up = mg_build_up(args)
-      last_mg = build_up.last.last
-      last_drinking_hour = build_up.last.first
-      break_down = mg_break_down(args.merge({
-        starting_mg: last_mg,
-        starting_hour: last_drinking_hour
-      }))
+    def initialize
+      @current_mg = args[:current_mg] || 0
+      @milligrams = args[:milligrams]
+      @hours = args[:hours]
+      @interval_count = args[:interval_count] || 49
+    end
 
+    def mg_series
+      build_up = mg_build_up
+      break_down = mg_break_down
       build_up.pop
       build_up + break_down
     end
@@ -46,18 +39,16 @@ class SubstanceCalc
     private
 
     # Returns a mg series during the time you are continually consuming a substance
-    def mg_build_up(args)
-      current_mg = args[:starting_mg] || 0
-      milligrams = args[:milligrams]
-      hours = args[:hours]
-      interval_count = (hours / 0.25).to_i
-      consumption_rate = milligrams / interval_count.to_f
-      mg_series = [[0, current_mg]]
+    def mg_build_up
+      temp_current_mg = @current_mg
+      build_up_interval_count = (@hours / 0.25).to_i
+      consumption_rate = @milligrams / build_up_interval_count.to_f
+      mg_series = [[0, @current_mg]]
 
-      interval_count.times do |interval|
-        current_mg = (current_mg + consumption_rate) - metabolized(current_mg)
-        current_mg = 0 if current_mg < 0
-        mg_series << [interval * 0.25 + 0.25, current_mg.round(@precision)]
+      build_up_interval_count.times do |interval|
+        temp_current_mg = (temp_current_mg + consumption_rate) - metabolized(temp_current_mg)
+        temp_current_mg = 0 if temp_current_mg < 0
+        mg_series << [interval * 0.25 + 0.25, temp_current_mg.round(@precision)]
       end
 
       mg_series
@@ -67,10 +58,9 @@ class SubstanceCalc
     def mg_break_down(args)
       starting_mg = args[:starting_mg] || 0
       starting_hour = args[:starting_hour] || 0
-      interval_count = args[:interval_count] || 49
       mg_series = []
 
-      interval_count.times do |interval|
+      @interval_count.times do |interval|
         hours = interval * 0.25
         next_mg = next_mg(starting_mg, hours)
         mg_series << [starting_hour + hours, next_mg]
@@ -90,5 +80,11 @@ class SubstanceCalc
       next_mg < 0 ? 0 : next_mg
     end
 
-  end
+    def last_mg
+      mg_build_up.last.last
+    end
+
+    def last_drinking_hour
+      mg_build_up.last.first
+    end
 end
