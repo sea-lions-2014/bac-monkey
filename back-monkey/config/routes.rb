d3596223938f1a3d046fd19103bfd0ddb624d4d1
Patrick Vilhena <null>
2014-02-28 17:59:36 -0800

    updated controllers to account for new model format

--- back-monkey/config/routes.rb
+++ back-monkey/config/routes.rb
@@ -4,5 +4,6 @@ BackMonkey::Application.routes.draw do
 
   post '/alcoholcalc/create.json', to: 'alcohol_calc#create'
   post '/caffeinecalc/create.json', to: 'caffeine_calc#create'
+  post '/nicotine/create.json', to: 'nicotine_calc#create'
 
 end
