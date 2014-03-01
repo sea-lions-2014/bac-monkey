010559629e25b90e26ee6945c7a4e128950dd332
Patrick Vilhena <null>
2014-02-28 17:34:03 -0800

    updated homepage tests to test different forms

--- back-monkey/app/controllers/alcohol_calc_controller.rb
+++ back-monkey/app/controllers/alcohol_calc_controller.rb
@@ -2,11 +2,14 @@ class AlcoholCalcController < ApplicationController
   respond_to :json, only: [:create]
   def create
     args = params[:alcohol_calc]
+    p params
+
     args[:weight] = args[:weight].to_i
     args[:alcohol] = args[:drinks].to_i * 0.6
     args[:hours] = args[:hours].to_i
 
-    @bac = AlcoholCalc.bac_series(args)
+    alcohol_calc = AlcoholCalc.new
+    @bac = alcohol_calc.bac_series(args)
     render json: @bac
   end
 end
