b21765e3a0b7174c4045a9857cfe6e986f9d903f
Patrick <Patrick@Patricks-MacBook-Air.local>
2014-03-01 18:47:39 -0800

    Users and Sessions controller tests written and passing.

--- back-monkey/spec/spec_helper.rb
+++ back-monkey/spec/spec_helper.rb
@@ -1,4 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
+require 'simplecov'
+SimpleCov.start
+
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
@@ -6,6 +9,7 @@ require 'rspec/autorun'
 require 'factory_girl_rails'
 require 'database_cleaner'
 require 'shoulda-matchers'
+
 
 
 # Requires supporting ruby files with custom matchers and macros, etc,
