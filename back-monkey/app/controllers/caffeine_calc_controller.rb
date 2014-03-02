2282c5330e9f77672eb67862ebe35e6cce29bd67
John Olmsted <johnaolmsted@gmail.com>
2014-03-02 13:28:22 -0800

    Refactors Alcohol, Nicotine, CaffeineCalcControllers with #format_params
    Moves params formatting from #create to a helper method #formate_params

--- back-monkey/app/controllers/caffeine_calc_controller.rb
+++ back-monkey/app/controllers/caffeine_calc_controller.rb
@@ -2,14 +2,10 @@ class CaffeineCalcController < ApplicationController
   respond_to :json, only: [:create]
 
   def create
-    args = params[:caffeine_calc]
-    args[:hours] = args[:hours].to_i
-    args[:milligrams] = MgOfCaffeine.caffeine_content(args[:type], args[:cups].to_i)
-
+    args = format_params(params)
     save_consumption_record(args) if current_user
     caffeine_calc = CaffeineCalc.new(args)
     @caffeine_series = caffeine_calc.mg_series
-
     render json: @caffeine_series
   end
 
@@ -17,5 +13,12 @@ class CaffeineCalcController < ApplicationController
       current_user.consumption_records.build(substance: "caffeine", 
                                              amount: args[:milligrams],
                                              unit_of_measure: "mg")
+  end
+
+  def format_params(params)
+    args = params[:caffeine_calc]
+    args[:hours] = args[:hours].to_i
+    args[:milligrams] = MgOfCaffeine.caffeine_content(args[:type], args[:cups].to_i)
+    args
   end
 end
