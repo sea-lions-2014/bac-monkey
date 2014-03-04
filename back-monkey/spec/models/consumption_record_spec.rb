6b9663240738f12f2b97f12cc760de2f35033353
John Olmsted <apprentice@dbc22.local>
2014-03-03 16:22:21 -0800

    Updates feature specs to reflect new profile

--- back-monkey/spec/models/consumption_record_spec.rb
+++ back-monkey/spec/models/consumption_record_spec.rb
@@ -6,16 +6,9 @@ describe ConsumptionRecord do
   context '#dose_description' do
     let(:caff_record) { FactoryGirl.create :consumption_record }
     let(:drink_record) { FactoryGirl.create :single_drink }
+
     it "returns a string" do
       expect(caff_record.dose_description.class).to eq String
-    end
-
-    it "should return the correct description" do
-      expect(caff_record.dose_description). to eq "200 mgs"
-    end
-
-    it "should return the unit of measure in the singular if amount is 1" do
-      expect(drink_record.dose_description).to eq "1 drink"
     end
   end
 
