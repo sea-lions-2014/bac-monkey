e76d252abaf27fed8514d49adaac4ca3d6f3d04b
Patrick Vilhena <plvilhena@gmail.com>
2014-03-05 11:35:08 -0800

    adds case insensitive username matching

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,10 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_user_name(params[:user][:user_name])
+    username = params[:user][:user_name]
+    user = User.find(:first, :conditions => ["lower(user_name) = ?", username.downcase])
     if user.authenticate(params[:user][:password])
       login(user)
-      @user = user
       redirect_to user_path(user)
     else
       flash.notice = "That username or password is invalid."
