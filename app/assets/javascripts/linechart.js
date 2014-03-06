ea2a6f24ed8fec5e7b4eb232732580dd36f023bf
Patrick Vilhena <null>
2014-03-06 12:43:00 -0800

    fixes chart warning

--- app/assets/javascripts/linechart.js
+++ app/assets/javascripts/linechart.js
@@ -59,15 +59,13 @@ var Chart = (function(){
       return element.y;
     });
 
-    var maxValue = Math.max.apply(null, valuesArray)
-
-    if (maxValue === 'BAC'){
-      var max = chart.yAxis.domain()[1]
-      if(max > 0.42){
+    if (dataObj[0].key === 'BAC'){
+      var maxValue = Math.max.apply(null, valuesArray)
+      if(maxValue > 0.42){
         $('.drive').hide();
         $('.dead').show();
       }
-      else if(max > 0.08){
+      else if(maxValue > 0.08){
         $('.dead').hide();
         $('.drive').show();
       }
